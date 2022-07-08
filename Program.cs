73773870dae959750a1bb47f581859721bf98cfe
Pollyana <pollyanarocha416@gmail.com>
2022-07-07 22:37:52 -0300

    this(expecificar Faz referencia ao objeto da classe mae)

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿class Calculos{
+    public int v1;
+    public int v2;
+    public Calculos(int v1, int v2){
+    //(o v1 e v2 q ta fora) e o v1 e v2 q ta dentro
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+    
+    public int Somar(){
+        return v1 + v2;
+    }
+}
+
+class Program{
+    static void Main(){
+        Calculos c = new Calculos(10, 2);
+        Console.WriteLine(c.Somar());
+    }
+}
